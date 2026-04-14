b81f31176553f6387198a41ba853bd9baeb8da58
ValladaresCode <rvalladares-2024060@kinal.edu.gt>
2026-04-14 17:27:14 -0600

    Creacion de Nav

---
+++ Client-admin/src/app/layouts/DashboardPage.jsx
@@ -0,0 +1,9 @@
+import { DashboardContainer } from "../../shared/layout/DashboardContainer.jsx"
+
+export const DashboardPage = () => {
+  return (
+    <DashboardContainer>
+        
+    </DashboardContainer>
+  )
+}
